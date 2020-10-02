54cb0eb84b0b6f71045c8e8b472f3469cf31a8cb
Alina Soboleva <alina.soboleva.spb@gmail.com>
2020-10-02 07:26:16 +0300

    feat: add solution to typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == "" || arguments.length === 0 ) return 0;
+  let minimum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == "" || arguments.length === 0 ) return 0;
+  let maximum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maximum) {
+      maximum = array[i];
+    }
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == "" || arguments.length === 0 ) return 0;
+  let sum = 0;
+  let average;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    average = sum / array.length;
+  }
+  return average;
 }
